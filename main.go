f086d658b6ec7d3928aafa127637ebdc1b7fab69
Shaun Hubbard <shaunhubbard2013@icloud.com>
2022-06-27 16:11:43 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	rtctokenbuilder "github.com/AgoraIO/Tools/DynamicKey/AgoraDynamicKey/go/src/RtcTokenBuilder"
+	rtmtokenbuilder "github.com/AgoraIO/Tools/DynamicKey/AgoraDynamicKey/go/src/RtmTokenBuilder"
+	"github.com/gin-gonic/gin"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+type Token struct {
+	UID     uint32 `json:"uid"`
+	CHANNEL string `json:"channel"`
+	RTC     string `json:"token2"`
+	RTM     string `json:"rtm"`
+}
+
+func generateRTMToken(intUid uint32) string {
+	appID := os.Getenv("APP_ID")
+	appCertificate := os.Getenv("CERTIFICATE")
+	expireTimeInSeconds := uint32(24 * 60 * 60)
+	currentTimestamp := uint32(time.Now().UTC().Unix())
+	uid := strconv.FormatUint(uint64(intUid), 10)
+	expireTimestamp := currentTimestamp + expireTimeInSeconds
+
+	result, err := rtmtokenbuilder.BuildToken(appID, appCertificate, uid, rtmtokenbuilder.RoleRtmUser, expireTimestamp)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	return result
+}
+
+func generateRtcToken(int_uid uint32, channelName string, role rtctokenbuilder.Role) string {
+	appID := os.Getenv("APP_ID")
+	appCertificate := os.Getenv("CERTIFICATE")
+	tokenExpireTimeInSeconds := uint32(60 * 60 * 24)
+	result, err := rtctokenbuilder.BuildTokenWithUID(appID, appCertificate, channelName, int_uid, role, tokenExpireTimeInSeconds)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return result
+}
+
+func generateARandomUID() uint32 {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Uint32()
+}
+
+func getToken(c *gin.Context) {
+	uid := generateARandomUID()
+	channels, _ := c.GetQueryArray("channels[]")
+	tokens := make([]Token, len(channels))
+	for idx, channel := range channels {
+		rtcToken := generateRtcToken(uid, channel, rtctokenbuilder.RolePublisher)
+		rtmToken := generateRTMToken(uid)
+		sampleToken := Token{
+			UID: uid, RTC: rtcToken, CHANNEL: channel, RTM: rtmToken,
+		}
+		tokens[idx] = sampleToken
+	}
+
+	c.IndentedJSON(http.StatusOK, tokens)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/token", getToken)
+	router.Run("localhost:8080")
+}
